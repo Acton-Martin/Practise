20f8e13fe4d08880a6b7b6a18e8025e71b24660d
ActonMartin <wszcla@outlook.com>
2019-03-16 17:47:00 +0800

    \t

--- 九九乘法表.py
+++ 九九乘法表.py
@@ -2,7 +2,7 @@ i=1
 while i<=9:
     j=1
     while j<=i:                                #使用的j小于i来控制的列。
-        print("%d*%d=%d\t"%(j,i,j*i),end="")
+        print("%d*%d=%d\t"%(j,i,j*i),end="")   # \t是制表符的意思，使其对其
         j+=1
     print("")
     i+=1
