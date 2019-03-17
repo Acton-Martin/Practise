ab1399aa8880fc051ce2c05ad59cf5c01ad2a41c
ActonMartin <wszcla@outlook.com>
2019-03-17 20:26:54 +0800

    coutiue

---
+++ contiue.py
@@ -0,0 +1,39 @@
+'''
+i=1
+while i<=10:
+    i+= 1
+    print("-----")
+    if i==5:
+        continue
+    print(i)
+print("======")
+'''
+
+
+'''
+i=1
+while i<=10:
+
+    print("-----")
+    if i==90:
+        continue
+    i += 1
+    print(i)
+print("======")
+
+'''
+i=1
+while i<=10:
+    j=1
+    while j<=i:
+        print("*",end="")
+        j+=1
+    print("")
+    i+=1
+
+
+
+
+
+
+
