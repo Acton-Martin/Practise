98df0d01f03ae3e6d1f31fae4a24a41f925c9919
ActonMartin <wszcla@outlook.com>
2019-03-23 14:35:42 +0800

    列表的增删

---
+++ 列表.py
@@ -0,0 +1,14 @@
+#定义一个列表
+names=["niui","niuiss","niuiasas","nsiajsia"]
+print(names)
+#使用append加入一个数据到原来的列表的最后
+names.append("shabi")
+print(names)
+#或者使用insert添加内容
+names.insert(2,"FBI")
+print(names)
+#################上面的运行效果如下###############################
+#    ['niui', 'niuiss', 'niuiasas', 'nsiajsia']
+#    ['niui', 'niuiss', 'niuiasas', 'nsiajsia', 'shabi']
+#    ['niui', 'niuiss', 'FBI', 'niuiasas', 'nsiajsia', 'shabi']
+
